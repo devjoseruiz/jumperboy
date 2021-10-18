b879a672fe21274b29348b75e5132859c8e9cfef
José Ruiz <joseruiz@keemail.me>
2021-10-18 18:22:07 +0400

    Create a game start screen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,12 +8,36 @@ public class GameController : MonoBehaviour
     public float parallaxSpeed = 0.02f;
     public RawImage background;
     public RawImage platform;
+    public GameObject uiIdle;
+
+    public enum GameState {Idle, Playing};
+    public GameState gameState = GameState.Idle;
+
+    public GameObject player;
 
     // Update is called once per frame
     void Update()
     {
-        float finalSpeed = parallaxSpeed * Time.deltaTime;
-        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
-        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+        // Game not started
+        if (gameState == GameState.Idle)
+        {
+            if (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))
+            {
+                // Change game state
+                gameState = GameState.Playing;
+                // Hide idle screen
+                uiIdle.SetActive(false);
+                // Change player animation
+                player.SendMessage("UpdateState", "PlayerRun");
+            }
+        }
+
+        // Game started
+        else
+        {
+            float finalSpeed = parallaxSpeed * Time.deltaTime;
+            background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+            platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+        }
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator playerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateState(string state = null)
+    {
+        if (state != null)
+        {
+            playerAnimator.Play(state);
+        }
+    }
+}
