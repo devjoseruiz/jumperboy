a0edf857189ae07592a0d1bda2fd0cd81a7eb594
José Ruiz <joseruiz@keemail.me>
2021-10-19 02:16:47 +0200

    Create enemy object

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float velocity = 2f;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = Vector2.left * velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Destroyer")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
